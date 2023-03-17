ecc6992265e1e24210f4d40df3d8ee0e4cd140bf
blake-clancyworld <103262609+blake-clancyworld@users.noreply.github.com>
2023-03-17 11:30:24 -0400

    Update graphql.py

--- odoo_graphql/controllers/graphql.py
+++ odoo_graphql/controllers/graphql.py
@@ -1,31 +1,53 @@
 from odoo import http
 from odoo.http import request, content_disposition
-from odoo.exceptions import ValidationError
-import werkzeug
 import json
-
+from ..utils import handle_graphql
+from ..auth import authenticate_and_execute
 import logging
 
 _logger = logging.getLogger(__name__)
-
+_logger.setLevel(logging.INFO)
 
 class GraphQL(http.Controller):
     @http.route(
-        "/graphql", auth="public", type="http", website=True, sitemap=False, csrf=False,
+        "/graphql", auth="public", type="http", website=True, sitemap=False, csrf=False, methods=["POST"]
     )
-    def graphql(self):
-        # https://spec.graphql.org/June2018/#sec-Response-Format
-        query = request.httprequest.data.decode()  # request.graphqlrequest
-        response = request.env["graphql.handler"].handle_query(query)
-        payload = json.dumps(response, indent=4)
-        return payload
+    def graphql(self, **data):
+        data = json.loads(request.httprequest.data.decode())  # Read request data as JSON
+        query = data.get("query")
+        _logger.info(f"Received data: {data}")  # Add this line for debugging
+        _logger.info(f"Received query: {query}")  # Add this line for debuggin
+        variables = data.get("variables") or {}
+        operation_name = data.get("operationName")
+        auth = data.get("auth") or {}
+        
+        def query_with_context(request, user):
+            response = request.env["graphql.handler"].handle_query(query)
+            return json.dumps(response, default=str)
+
+        if auth:
+            result = authenticate_and_execute(query_with_context, auth)
+        else:
+            result = query_with_context(request, request.env.user)
+
+        return result
 
     @http.route(
-        "/graphiql", type="http", website=True, sitemap=False
+        "/graphql/schema",
+        auth="public",
+        type="http",
+        website=True,
+        sitemap=False,
+        csrf=False,
     )
-    def graphiql(self):
-        introspection = request.env["graphql.handler"].has_introspection()
-        if not introspection:
-            raise werkzeug.exceptions.NotFound()
-            # raise ValidationError("Introspection is not allowed")
-        return request.render("odoo_graphql.graphiql")
+    def graphql_schema(self):
+        # Nb: Not meant to be displayed
+        content = request.env["graphql.handler"].schema()
+        response = request.make_response(
+            content,
+            headers=[
+                ("Content-Type", "application/graphql"),
+                ("Content-Disposition", content_disposition("schema.graphql")),
+            ],
+        )
+        return response
